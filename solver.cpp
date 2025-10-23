7e37e40ec277aef0a8936290f85d6556b54b8e8f
santanu <santanu27102004mandal@gmail.com>
2025-10-23 09:23:15 +0530

    basic structure

---
+++ solver.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+# include<cmath>
+
+using namespace std;
+
+class complex{
+    double re, im;
+
+    complex(double r, double i){
+        re = r;
+        im = i;
+    }
+};
+
+double d_calcu(double a, double b, double c)
+{
+    return sqrt(abs(pow(b,2) - 4*a*c));
+}
+
+bool d_calcu(double a, double b, double c)
+
+int main()
+{
+    return 0;
+}
