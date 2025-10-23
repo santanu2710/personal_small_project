794a309639951fa7a058f0719df77007e6236d98
santanu <santanu27102004mandal@gmail.com>
2025-10-23 09:53:21 +0530

    root type function added

--- solver.cpp
+++ solver.cpp
@@ -10,16 +10,25 @@ class complex{
         re = r;
         im = i;
     }
-};
+};// complex type data structure for complex roots
 
 double d_calcu(double a, double b, double c)
 {
     return sqrt(abs(pow(b,2) - 4*a*c));
 }
 
-bool c_or_r(double a, double b, double c)
+bool type_of_roots(double a, double b, double c)
 {
+    // return true if root is real
+    // return false if root is complex
 
+    bool type = true;
+    if (pow(b,2) < 4*a*c)
+    {
+        type = false;
+    }
+
+    return type;
 }
 
 int main()
